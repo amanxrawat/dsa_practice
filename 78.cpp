426d10566d979cf11d6959d9a5cb4faf3cf4ff51
amanxrawat <amanxrawat9@gmail.com>
2023-09-08 21:33:35 +0530

    sovled the power set problem gives the power set of a list of arrays

---
+++ 78.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+void sets(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans ){
+    if(index >= nums.size()){
+        ans.push_back(output); 
+        return ;
+    }
+
+    // exclude
+    sets(nums,output,index+1,ans);
+    // include
+    int element = nums[index];
+    output.push_back(element);
+    sets(nums,output,index+1,ans);
+
+}
+vector<vector<int>> subsets(vector<int>& nums) {
+    
+    // power set
+    vector<vector<int>> ans;
+    vector<int> output;
+    int index = 0;
+    sets(nums,output,index,ans);  
+    return ans;
+}
+
+
+int main()
+{
+    
+    return 0;
+}
+
+
+
+
+
+
+/*Given an integer array nums of unique elements, return all possible 
+subsets
+ (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+*/
